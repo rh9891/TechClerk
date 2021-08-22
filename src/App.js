8ee030f473ff194be8f93f20089e8dd5d88644fe
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-08-21 20:32:40 -0400

    Completed Materialize setup to application. Set up proxy.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
 
 function App() {
   return <h1>Welcome to Tech Clerk!</h1>;
