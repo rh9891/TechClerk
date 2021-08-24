f6f63b97f487ade5ca5dad347090e1337d5293ea
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-08-23 22:26:51 -0400

    Initialized the Materialize JS and completed setup.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
 import "./App.css";
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    // Initializes Materialize JS.
+    M.AutoInit();
+  }, []);
   return <h1>Welcome to Tech Clerk!</h1>;
-}
+};
 
 export default App;
