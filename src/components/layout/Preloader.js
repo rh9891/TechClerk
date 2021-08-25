d42ffce2fceb76635e5f1928ed911bd016787403
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-08-24 22:33:39 -0400

    Created a user feedback (preloader component) in case the information for the system logs takes too long to fetch.

---
+++ src/components/layout/Preloader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Preloader = () => {
+  return (
+    <div className="progress teal lighten-3">
+      <div className="indeterminate teal"></div>
+    </div>
+  );
+};
+
+export default Preloader;
