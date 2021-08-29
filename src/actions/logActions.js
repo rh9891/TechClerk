c7dba4d31d42db292a82a75359bcb0aee7c32650
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-08-29 16:51:27 -0400

    Created an action and reducer for adding a log entry. Application is working as expected.

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "./types";
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from "./types";
 
 // Gets logs from the server.
 export const getLogs = () => async (dispatch) => {
@@ -10,6 +10,32 @@ export const getLogs = () => async (dispatch) => {
 
     dispatch({
       type: GET_LOGS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.response.data,
+    });
+  }
+};
+
+// Adds new log entry.
+export const addLog = (log) => async (dispatch) => {
+  try {
+    setLoading();
+
+    const res = await fetch("./logs", {
+      method: "POST",
+      body: JSON.stringify(log),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+
+    dispatch({
+      type: ADD_LOG,
       payload: data,
     });
   } catch (error) {
--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -31,6 +31,7 @@ const Logs = ({ log: { logs, loading }, getLogs }) => {
 
 Logs.propTypes = {
   log: PropTypes.object.isRequired,
+  getLogs: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
