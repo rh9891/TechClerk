a140862b4678a13e98f9bc99102401962f5b4a09
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-08-29 16:37:25 -0400

    Made changes to the formatting of the add button. Clearer functionality.

--- src/components/layout/AddButton.js
+++ src/components/layout/AddButton.js
@@ -2,12 +2,9 @@ import React from "react";
 
 const AddButton = () => {
   return (
-    <div className="fixed-action-btn">
-      <a
-        href="#add-log-modal"
-        className="btn-floating btn-large blue-grey modal-trigger"
-      >
-        <i className="large material-icons">add</i>
+    <div className="fixed-action-btn click-to-toggle">
+      <a href="#!" className="btn-floating btn-large blue-grey">
+        <i className="large material-icons">more_horiz</i>
       </a>
       <ul>
         <li>
@@ -15,7 +12,7 @@ const AddButton = () => {
             href="#tech-list-modal"
             className="btn-floating blue-grey modal-trigger"
           >
-            <i className="material-icons">person</i>
+            <i className="material-icons">people</i>
           </a>
         </li>
         <li>
@@ -24,6 +21,14 @@ const AddButton = () => {
             className="btn-floating blue-grey modal-trigger"
           >
             <i className="material-icons">person_add</i>
+          </a>
+        </li>
+        <li>
+          <a
+            href="#add-log-modal"
+            className="btn-floating blue-grey modal-trigger"
+          >
+            <i className="material-icons">note_add</i>
           </a>
         </li>
       </ul>
