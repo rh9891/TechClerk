ea7406cf7afc382ea59a3c7bcc3ec3c662e79ec2
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-08-30 14:32:06 -0400

    Completed full functionality of application using the JSON-SERVER as a mock backend.

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -53,6 +53,27 @@ export const addTech = (tech) => async (dispatch) => {
   }
 };
 
+// Deletes tech from server.
+export const deleteTech = (id) => async (dispatch) => {
+  try {
+    setLoading();
+
+    await fetch(`/techs/${id}`, {
+      method: "DELETE",
+    });
+
+    dispatch({
+      type: DELETE_TECH,
+      payload: id,
+    });
+  } catch (error) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: error.response.statusText,
+    });
+  }
+};
+
 // Sets loading to true.
 export const setLoading = () => {
   return {
