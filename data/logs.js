1907e6a4b7ca26e8b9ab30353f22f65a8a125570
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2021-08-31 07:56:08 -0400

    Getting ready to create a backend for the application. Put the react files into a client folder. Created data files to be used later for seeding.

---
+++ data/logs.js
@@ -0,0 +1,44 @@
+const logs = [
+  {
+    id: 1,
+    message: "Changed network card in Unit 340.",
+    attention: false,
+    tech: "Ken Miller",
+    date: "2021-08-30T17:23:19.373Z",
+  },
+  {
+    id: 2,
+    message: "Fixed the hard drive of Unit 502.",
+    attention: false,
+    tech: "Sam Weir",
+    date: "2021-08-29T23:04:13.032Z",
+  },
+  {
+    id: 3,
+    message: "Unit 110 is failing to make connection to wireless network.",
+    attention: true,
+    tech: "Ken Miller",
+    date: "2021-08-29T20:56:46.909Z",
+  },
+  {
+    id: 4,
+    message: "Unit 192 screen is blank after log-in.",
+    attention: true,
+    tech: "Daniel Desario",
+    date: "2021-08-30T23:59:40.216Z",
+  },
+  {
+    id: 5,
+    message: "The internet connection on Unit 201 is slow.",
+    attention: true,
+    tech: "Bill Haverchuck",
+    date: "2021-08-31T00:00:40.714Z",
+  },
+  {
+    id: 6,
+    message: "Rebooted the operating system on Unit 138.",
+    attention: false,
+    tech: "Sam Weir",
+    date: "2021-08-31T00:01:20.949Z",
+  },
+];
